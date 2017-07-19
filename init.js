0619a30165f330a4ce893d0adf8c0d8c5ed6181a
christikaes <christikaes@gmail.com>
2017-07-19 03:18:18 -0400

    Part2-RegisterServiceWorker

---
+++ init.js
@@ -0,0 +1,9 @@
+(function(){
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+            .register("./sw.js")
+            .then(function() {
+                console.log("[Service Worker] Registered");
+            });
+    }
+})()
